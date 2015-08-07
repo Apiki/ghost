9256182ce4721f7c751437c772ba65b31227298f
elvis.pereira <elvis.pereira@apiki.com>
2015-08-07 10:57:58 -0300

    Change identation

--- ghost/assets/javascripts/app/component-icons.js
+++ ghost/assets/javascripts/app/component-icons.js
@@ -1,10 +1,10 @@
 Module.ComponentWrapper( 'Icons', function(Icons, Utils, $) {
 
-	Icons.fn.init = function() {
+    Icons.fn.init = function() {
         $.getJSON( '../assets/fonts/selection.json', function(data) {
             this.render( data );
         }.bind( this ) );
-	};
+    };
 
     Icons.fn.render = function(data) {
         this.elements.list.html( MONKEY.Templates.icons( data ) );
