3ac1add84cf9aa2097cf5e4927749d0d2fa9dd55
elvishp2006 <elvishp2006@gmail.com>
2015-08-07 10:57:58 -0300

    Change identation

--- ghost/assets/javascripts/boot.js
+++ ghost/assets/javascripts/boot.js
@@ -1,13 +1,13 @@
 jQuery(function() {
-	var context = jQuery( 'body' )
-	  , route   = context.data( 'route' )
-	;
+    var context = jQuery( 'body' )
+      , route   = context.data( 'route' )
+    ;
 
-	MONKEY.vars = {
-		body  : context,
-		route : route
-	};
+    MONKEY.vars = {
+        body  : context,
+        route : route
+    };
 
-	//set route in application
-	Dispatcher( MONKEY.Application, route, [context] );
+    //set route in application
+    Dispatcher( MONKEY.Application, route, [context] );
 });
