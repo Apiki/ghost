107238277478607f0a317e2835a4d87b09925c5d
elvis.pereira <elvis.pereira@apiki.com>
2015-08-14 14:04:05 -0300

    Ajustment on menu.

--- ghost/assets/javascripts/templates/templates.js
+++ ghost/assets/javascripts/templates/templates.js
@@ -59,7 +59,7 @@ this["MONKEY"]["Templates"]["menu"] = Handlebars.template({"1":function(depth0,h
     var stack1;
 
   return "<ul>\n"
-    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.sections : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "</ul>";
 },"useData":true,"useDepths":true});
 
